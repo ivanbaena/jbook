151f219086db8f2f4d9d54cd4a0688fe29b16808
Ivan Baena <baenadev.io@gmail.com>
2021-03-13 00:48:14 -0500

    add monaco-editor to project

---
+++ src/components/code-editor.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import MonacoEditor from '@monaco-editor/react';
+
+interface CodeEditorProps {
+  initialValue: string;
+  onChange(value: string): void;
+}
+const CodeEditor: React.FC<CodeEditorProps> = ({ initialValue, onChange }) => {
+  const onEditorDidMount = (getValue: () => string, monacoEditor: any) => {
+    monacoEditor.onDidChangeModelContent(() => {
+      onChange(getValue());
+    });
+  };
+  return (
+    <MonacoEditor
+      editorDidMount={onEditorDidMount}
+      value={initialValue}
+      height='500px'
+      language='javascript'
+      theme='dark'
+      options={{
+        wordWrap: 'on',
+        minimap: { enabled: false },
+        showUnused: false,
+        folding: false,
+        lineNumbersMinChars: 3,
+        fontSize: 16,
+        scrollBeyondLastLine: false,
+        automaticLayout: true,
+      }}
+    />
+  );
+};
+
+export default CodeEditor;
